39256c03b0b76929687d1835ccece24e202bb895
VictorHCamargo <victor.h0786@gmail.com>
2026-03-17 10:34:15 -0300

    Ajustes

--- frontend/src/app/features/pages/chatbot/chatbot.ts
+++ frontend/src/app/features/pages/chatbot/chatbot.ts
@@ -1,18 +1,23 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { IChatConfig } from './interfaces/chat-config';
 import { Conversation } from './components/conversation/conversation';
-import { FieldTree } from '@angular/forms/signals';
+import { FieldTree, form, FormField } from '@angular/forms/signals';
 import { IFormChatConfig } from './interfaces/form-chat-config';
+import { ConversationService } from './components/conversation/services/conversation-service';
 
 
 @Component({
   selector: 'app-chatbot',
-  imports: [Conversation],
+  imports: [Conversation, FormField],
   templateUrl: './chatbot.html',
   styleUrl: './chatbot.scss',
 })
 export class Chatbot {
-  fieldForm! :FieldTree<IFormChatConfig>;
+  conversationService = inject(ConversationService);
+  formModel : WritableSignal<IFormChatConfig> = signal<IFormChatConfig>({
+    prompt : ''
+  });
+  fieldForm :FieldTree<IFormChatConfig> = form(this.formModel);
 
   chatConfig : WritableSignal<IChatConfig> = signal<IChatConfig>({
     name : "Steve",
@@ -26,7 +31,8 @@ export class Chatbot {
     wayImg : "https://placehold.co/40"
   })
 
-  onInput() {
-    throw Error('method not implement')
+  onClick() {
+    // TO-DO: Melhorar a forma que o input é enviado, além de adicionar retrições caso o joão deseja
+    this.conversationService.set(this.fieldForm.prompt().value())
   }
 }
--- frontend/src/app/features/pages/chatbot/interfaces/form-chat-config.ts
+++ frontend/src/app/features/pages/chatbot/interfaces/form-chat-config.ts
@@ -1,3 +1,3 @@
 export interface IFormChatConfig {
-    input : string
+    prompt : string
 }
